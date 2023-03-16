7c19e0da9c3a8958f358f52f833459d5bb7f5e11
Abhishek Kumar Upadhyay <119941138+abhishek7408@users.noreply.github.com>
2023-03-17 01:03:04 +0530

    Update EPL SQL Queries.sql

--- EPL SQL Queries.sql
+++ EPL SQL Queries.sql
@@ -3,7 +3,31 @@
 --Date of Creation: 26/02/2023
 --Tools Used: PostgreSQL
 
-/* About The Dataset- */
+/* About The Dataset-
+This dataset is a collection of basic and vital stats for the 2020-21 English Premier League season. The dataset includes every player who has played in the EPL and 
+standard stats like Goals, Assists, xG, xA, Passes_Attempted and Pass Accuracy and more. There are 532 Row and 19 Columns.
+
+Content-
+
+Position- Each player has a certain position, in which he plays regularly. The position in this dataset are, FW - Forward, MF - Midfield, DF - Defensive, GK - Goalkeeper
+
+Starts- The number of times the player was named in the starting 11 by the manager.
+
+Mins- The number of minutes played by the player.
+
+Goals- The number of Goals scored by the player.
+
+Assists- The number of times the player has assisted other player in scoring the goal.
+
+Passes_Attempted- The number of passes attempted by the player.
+
+Perc_Passes_Completed- The number of passes that the player accurately passed to his teammate.
+
+xG- Expected number of goals from the player in a match.
+
+xA- Expected number of assists from the player in a match.
+
+Yellow_Cards- The players get a yellow card from the referee for indiscipline, technical fouls, or other min*/
 
 -- Question(1)- Create a table named EPL_Analysis.
 
